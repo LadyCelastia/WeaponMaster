a6534e8b7b4f5af8691e1595811a771ebd3576e8
LadyCelestia <140333669+LadyCelastia@users.noreply.github.com>
2023-09-03 00:58:21 +0800

    Update BlockParry.lua

--- WeaponMaster/Composite/BlockParry.lua
+++ WeaponMaster/Composite/BlockParry.lua
@@ -13,14 +13,25 @@ BlockParry.new = function(fields : {any?}?)
 	
 	local self = setmetatable({}, BlockParry)
 	
+	self.OwnerCharacterStats = nil
+	self.CanParryValue = nil
+	
 	function self:Block(this)
 		if this.Owner ~= nil then
 			if this.Owner:FindFirstChild("Blocking") == nil and this.Owner:FindFirstChild("TrueStun") == nil then
-				local Parry = Instance.new("BoolValue")
-				Debris:AddItem(Parry, 0.2)
-				Parry.Name = "Parrying"
-				Parry.Value = true
-				Parry.Parent = this.Owner
+				if this.CanParryValue ~= nil then
+					if this.CanParryValue.Value == true then
+						this.CanParryValue.Value = false
+						local Parry = Instance.new("BoolValue")
+						Debris:AddItem(Parry, 0.2)
+						Parry.Name = "Parrying"
+						Parry.Value = true
+						Parry.Parent = this.Owner
+						task.delay(4, function()
+							this.CanParryValue.Value = true
+						end)
+					end
+				end
 				local Block = Instance.new("BoolValue")
 				Block.Name = "Blocking"
 				Block.Value = true
